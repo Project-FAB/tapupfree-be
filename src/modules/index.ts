1297b91609e5c1c4a8d40f62a16d24ffdc178afc
Fritz <bathan0808fritz@gmail.com>
2024-06-07 01:15:25 +0800

    feat: health check

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
 import users from './users/users.route';
-
+import health from './health-check/health.route';
 const router: Router = Router();
 
 router.use('/users', users);
+router.use('/health', health);
 // router.use("/projects", projects);
 
 export default router;
